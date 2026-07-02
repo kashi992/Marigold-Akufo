eb584e9f9e619120356c3f87c838e6d4d91d5b20
kashi992 <kashifrajput992@gmail.com>
2026-07-02 18:08:01 +0500

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -342,7 +342,7 @@ export default function Home({ navigateTo }) {
         <div className="home-hero-bg" style={{ backgroundImage: `url('/images/hero.jpg')` }} />
         <div className="home-hero-center">
           <p className="home-hero-title">
-            <SplitChars text="marigold akufo-addo66" />
+            <SplitChars text="marigold akufo-addo" />
           </p>
           <div className="home-hero-line" />
         </div>
